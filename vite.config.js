5168cb88416aa8e2c8eb50fb04615366b033c050
chetkiy1988 <chetkiy1988@gmail.com>
2024-02-28 18:14:12 +0200

    gh pages add

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/chetkiy777/resume2024",
   plugins: [
     react(),
     svgr({
